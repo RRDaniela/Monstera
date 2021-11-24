ad70c2ff5526cbc781e1abb9851bd2d6e5d614ee
Daniela Rodríguez <32688524+RRDaniela@users.noreply.github.com>
2021-11-24 09:59:19 -0600

    Creación del login y registro
    Se creó una vista para el registro e inicio de sesión de usuarios.

--- js/main.js
+++ js/main.js
@@ -39,3 +39,26 @@ function scrollHeader(){
         header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader);
+
+//Mostrar menú
+const signUp=document.getElementById('sign-up'),
+        signIn=document.getElementById('sign-in'),
+        loginIn=document.getElementById('login-in'),
+        loginUp=document.getElementById('login-up');
+
+//cambiar a registrarse o a iniciar sesión
+signUp.addEventListener('click', ()=>{
+    loginIn.classList.remove('block')
+    loginUp.classList.remove('none')
+
+    loginIn.classList.add('none')
+    loginUp.classList.add('block')
+})
+
+signIn.addEventListener('click', ()=>{
+    loginIn.classList.remove('none')
+    loginUp.classList.remove('block')
+
+    loginIn.classList.add('block')
+    loginUp.classList.add('none')
+})
